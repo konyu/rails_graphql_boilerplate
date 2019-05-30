7f1e29764046e7e3bf8ebd48c0985085b01cfb30
kon_yu <yusukedrive@gmail.com>
2019-05-30 21:08:03 +0900

    Add: mutation sample

--- app/graphql/types/user_type.rb
+++ app/graphql/types/user_type.rb
@@ -1,7 +1,7 @@
 module Types
   class UserType < BaseObject
     field :id, ID, null: false
-    field :email, String, null: false
-    field :api_token, String, null: false
+		field :email, String, null: false
+		field :api_token, String, null: true
   end
 end
